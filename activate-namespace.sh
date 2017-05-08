c61b7be52820ed19683176f29c86cd06936cbeb2
Dmitry Bashkatov <dbashkatov@gmail.com>
2017-05-09 00:10:10 +0300

    improve activate-namespace.sh script

--- activate-namespace.sh
+++ activate-namespace.sh
@@ -2,7 +2,36 @@
 
 set -e
 
-kube_namespace=$1
+source_namespace=ceph
+target_namespace=ceph
+
+while [[ $# -gt 0 ]]; do
+    case "$1" in
+        -s|--source-namespace)
+            source_namespace="$2"
+            shift
+        ;;
+        -t|--target-namespace)
+            target_namespace="$2"
+            shift
+        ;;
+        -d|--debug)
+            conf_file=/etc/squid/debug.conf
+        ;;
+        -h|--help)
+            echo "-s|--source-namespace: namespace from where to copy the secret"
+            echo "-t|--target-namespace: namespace where to copy the secret"
+            echo "-h|--help: this help"
+            exit 0
+        ;;
+        *)
+            echo "unknown option: $1" >&2
+            exit 1
+        ;;
+    esac
+
+    shift
+done
 
 {
   cat <<EOF
@@ -13,6 +42,6 @@ metadata:
 type: kubernetes.io/rbd
 data:
   key: |
-    $(kubectl get secret rbd-user --namespace=ceph -o json | jq -r '.data | .[]')
+    $(kubectl get secret rbd-user --namespace=${source_namespace} --template='{{ .data.key }}')
 EOF
-} | kubectl create --namespace ${kube_namespace} -f -
+} | kubectl create --namespace ${target_namespace} -f -
